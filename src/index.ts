f993431e28a28b814f7926ed0df87d95b762ea71
Mathieu Eveillard <mathieu.eveillard@gmail.com>
2023-02-20 12:27:07 +0100

    Refactoring

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,15 @@ export type Point = {
   y: number;
 };
 
+const computeOneDimensionDistance = (a: number, b: number) => {
+  return Math.abs(b - a);
+};
+
 const computeManhattanDistance = (a: Point, b: Point): number => {
-  return Math.abs(b.x - a.x) + Math.abs(b.y - a.y);
+  return (
+    computeOneDimensionDistance(b.x, a.x) + //
+    computeOneDimensionDistance(b.y, a.y)
+  );
 };
 
 export default computeManhattanDistance;
